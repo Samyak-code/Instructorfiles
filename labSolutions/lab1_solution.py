847381a344f12280b26ca6a630c2f5e3789e7391
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-07-05 23:37:56 -0700

    Copied over new solutions

--- labSolutions/lab1_solution.py
+++ labSolutions/lab1_solution.py
@@ -1,45 +1,40 @@
 """
-Copyright Harvey Mudd College
+Copyright MIT and Harvey Mudd College
 MIT License
-Fall 2019
+Summer 2020
 
-Lab 1 - Driving and Controller
+Lab 1 - Driving in Shapes
 """
 
-################################################################################
+########################################################################################
 # Imports
-################################################################################
+########################################################################################
 
 import sys
 
-sys.path.insert(0, "../../library")
-from racecar_core import *
+sys.path.insert(1, "../../library")
+import racecar_core
 import racecar_utils as rc_utils
 
-rospy.init_node("racecar")
-
-
-################################################################################
+########################################################################################
 # Global variables
-################################################################################
+########################################################################################
 
-rc = Racecar()
+rc = racecar_core.create_racecar()
 
 # A queue of driving steps to execute
-# Each entry is a triple of the form (time remaining, speed, angle)
+# Each entry is a list containing (time remaining, speed, angle)
 queue = []
 
-################################################################################
+########################################################################################
 # Functions
-################################################################################
+########################################################################################
 
 
 def start():
     """
     This function is run once every time the start button is pressed
     """
-    global queue
-
     # Begin at a full stop
     rc.drive.stop()
 
@@ -47,11 +42,11 @@ def start():
     queue.clear()
 
     # Print start message
-    # TODO: add a line explaining what the Y button does
+    # TODO (main challenge): add a line explaining what the Y button does
     print(
         ">> Lab 1 - Driving in Shapes\n"
         "\n"
-        "Controlls:\n"
+        "Controls:\n"
         "   Right trigger = accelerate forward\n"
         "   Left trigger = accelerate backward\n"
         "   Left joystick = turn front wheels\n"
@@ -83,16 +78,16 @@ def update():
     # TODO: Drive in a shape of your choice when the Y button is pressed
 
     # Calculate speed from triggers
-    forwardSpeed = rc.controller.get_trigger(rc.controller.Trigger.RIGHT)
-    backSpeed = rc.controller.get_trigger(rc.controller.Trigger.LEFT)
-    speed = forwardSpeed - backSpeed
+    right_trigger = rc.controller.get_trigger(rc.controller.Trigger.RIGHT)
+    left_trigger = rc.controller.get_trigger(rc.controller.Trigger.LEFT)
+    speed = right_trigger - left_trigger
 
     # Calculate angle from left joystick
     angle = rc.controller.get_joystick(rc.controller.Joystick.LEFT)[0]
 
     # If the triggers or joystick were pressed, clear the queue to cancel the current
     # shape and allow for manual driving
-    if forwardSpeed > 0 or backSpeed > 0 or angle > 0:
+    if right_trigger > 0 or right_trigger > 0 or angle > 0:
         queue.clear()
 
     # If the queue is not empty, follow the current drive instruction
@@ -105,61 +100,66 @@ def update():
 
     rc.drive.set_speed_angle(speed, angle)
 
+    # TODO (main challenge): Drive in a shape of your choice when the Y button
+    # is pressed
+
 
 def drive_circle():
     """
-    Add steps to drive in a circle to the instruction queue
+    Add steps to drive in a circle to the instruction queue.
     """
     global queue
 
     # Tune this constant until the car completes a full circle
-    CIRCLE_TIME = 5
+    CIRCLE_TIME = 6
 
     queue.clear()
 
     # Turn right at full speed
-    queue.append((CIRCLE_TIME, 1, 1))
+    queue.append([CIRCLE_TIME, 1, 1])
 
 
 def drive_square():
     """
-    Add steps to drive in a square to the instruction queue
+    Add steps to drive in a square to the instruction queue.
     """
     global queue
 
     # Tune these constants until the car completes a clean square
-    STRAIGHT_TIME = 1
-    TURN_TIME = 1.5
+    STRAIGHT_TIME = 1.5
+    TURN_TIME = 1.2
 
     queue.clear()
 
     # Repeat 4 copies of: drive straight, turn right
-    for i in range(1, 4):
-        queue.append((STRAIGHT_TIME, 1, 0))
-        queue.append((TURN_TIME, 1, 1))
+    for i in range(0, 4):
+        queue.append([STRAIGHT_TIME, 1, 0])
+        queue.append([TURN_TIME, 1, 1])
 
 
 def drive_figure_eight():
     """
-    Add steps to drive in a figure eight to the instruction queue
+    Add steps to drive in a figure eight to the instruction queue.
     """
     global queue
 
     # Tune these constants until the car completes a clean figure eight
-    STRAIGHT_TIME = 1
-    TURN_TIME = 4
+    STRAIGHT_TIME_1 = 3  # The first straight time is longer to give time to accelerate
+    STRAIGHT_TIME_2 = 2
+    TURN_TIME = 3.5
 
     queue.clear()
 
-    # Repeat 2 copies of: drive straight, turn right
-    for i in range(1, 2):
-        queue.append((STRAIGHT_TIME, 1, 0))
-        queue.append((TURN_TIME, 1, 1))
+    # Take the following steps: straight, right, straight, left
+    queue.append([STRAIGHT_TIME_1, 1, 0])
+    queue.append([TURN_TIME, 1, 1])
+    queue.append([STRAIGHT_TIME_2, 1, 0])
+    queue.append([TURN_TIME, 1, -1])
 
 
-################################################################################
-# Do not modify any code beyond this point
-################################################################################
+########################################################################################
+# DO NOT MODIFY: Register start and update and begin execution
+########################################################################################
 
 if __name__ == "__main__":
     rc.set_start_update(start, update)
